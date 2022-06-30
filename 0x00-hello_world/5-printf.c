f61aa0ef2cd436037baa8883319d137624b3e1da
Tr-reny <rennykipkoech@gmail.com>
2022-06-30 10:39:28 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash 
+#include <stdio.h>
+/**
+ *  *main - Entry point
+ *   *My hello, printf task
+ *    *Return: return 1 after program execution
+ *     */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
