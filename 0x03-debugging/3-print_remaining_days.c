753109e503307dc12f4a967db520bc4d6beb3880
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2022-07-06 14:25:50 -0400

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,7 +3,7 @@
 /**
 * print_remaining_days - takes a date and prints how many days are
 * left in the year, taking leap years into account
-* @month: month in number  format
+* @month: month in number format
 * @day: day of month
 * @year: year
 * Return: void
