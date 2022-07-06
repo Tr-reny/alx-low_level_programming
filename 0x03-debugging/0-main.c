2b2fdd9968c35bc226fea79d16b80da8446feefb
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2022-07-06 21:27:11 +0300

    0-main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- *  * main - main
- *   * Return: 0
- *    */
+ * main - main
+ * Return: 0
+ */
 
 int main(void)
 {
-	int i;
-	i = 0;
+int i;
+i = 0;
 
-	positive_or_negative(i);
+positive_or_negative(i);
 
-	return (0);
+return (0);
 
 }
