52e4b9c6bfbd7dac7feead4190c73aed31018916
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2022-09-30 12:09:10 +0300

    sum node

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - find sum of elements in the list
+ * @head: head of linked list
+ * Return: sum
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+	dlistint_t *next;
+	int sum = 0;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		sum += current->n;
+		current = next;
+	}
+	return (sum);
+}
